11f7b87392dd85964503d99b44f740e0172633a9
lev.pozniakoff <lev.pozniakoff@kudelskisecurity.com>
2025-10-09 22:47:11 +0200

    bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod sorting;
+use crate::sorting::bubble_sort::test_bubble_sort;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec = vec![3, 9, 12, 8, 0, 78, 539238472, 2];
+    let mut vec2 = vec![1];
+    let mut vec3: Vec<u32> = vec![];
+    test_bubble_sort(&mut vec);
+    test_bubble_sort(&mut vec2);
+    test_bubble_sort(&mut vec3);
 }
---
+++ src/sorting/bubble_sort.rs
@@ -0,0 +1,31 @@
+
+
+fn swap_values(vec: &mut Vec<u32>, rhs_idx: usize, lhs_idx: usize) {
+    let tmp = vec[rhs_idx];
+    vec[rhs_idx] = vec[lhs_idx];
+    vec[lhs_idx] = tmp;
+
+}
+
+fn bubble_sort(vec: &mut Vec<u32>) -> u32 {
+    if vec.len() <= 1 {
+        return 0
+    }
+    let mut iterations = 0;
+    for index in 0..vec.len(){
+        for pivot in index..vec.len() {
+            if vec[index] > vec[pivot] {
+                swap_values(vec, index, pivot);
+            }
+            iterations += 1;
+        }
+    }
+    iterations
+}
+
+pub fn test_bubble_sort(vec: &mut Vec<u32>) {
+    println!("Before {:?}", vec);
+    let it = bubble_sort(vec);
+    println!("After {:?}", vec);
+    println!("Iteration {}", it)
+}
